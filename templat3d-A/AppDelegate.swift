1c123fc45f3dc969f7b41fd0ea36794d461f51b8
andrew hazlett <andyhaz1970@gamile.com>
2021-09-17 14:06:51 -0500

    Initial Commit

---
+++ templat3d-A/AppDelegate.swift
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.swift
+//  templat3d-A
+//
+//  Created by andyhaz on 9/17/21.
+//
+
+import Cocoa
+
+@main
+class AppDelegate: NSObject, NSApplicationDelegate {
+    
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+    
+}
